a126547b6b57a08b5721b31a0dcab5c8d766fb6e
Daminimm <daminim.verve@gmail.com>
2024-04-25 17:43:37 +0530

    create view complete

---
+++ TASKTRACKERSYSTEM/Controllers/EMPOLYEEController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TASKTRACKERSYSTEM.Models;
+namespace TASKTRACKERSYSTEM.Controllers
+{
+    public class EMPOLYEEController : Controller
+    {
+        // GET: EMPOLYEE
+        
+        public ActionResult Index()
+        {
+           TASKTRACKEREntities DBContext = new TASKTRACKEREntities();
+           List<TASK> EmpList = DBContext.TASKs.ToList();
+           return View(EmpList);
+        }
+        public ActionResult Create()
+        {
+            TASKTRACKEREntities DBContext = new TASKTRACKEREntities();
+
+            List<USER> UserList = DBContext.USERS.ToList();
+            if (UserList == null)
+            {
+                
+                return RedirectToAction("Index");
+
+            }
+           
+            ViewBag.UserList = UserList.Select(x => new SelectListItem { Value = x.UserID.ToString(), Text = x.Username.ToString() }).ToList();
+            return View(new TASK());
+
+        }
+        [HttpPost]
+        public ActionResult Create(TASK MODEL)
+        {
+            TASKTRACKEREntities DBContext = new TASKTRACKEREntities();
+            List<USER> UserList = DBContext.USERS.ToList();
+            ViewBag.UserList = UserList.Select(x => new SelectListItem { Value = x.UserID.ToString(), Text = x.Username.ToString() }).ToList();
+            if (ModelState.IsValid)
+            {
+                
+                DBContext.TASKs.Add(MODEL);
+                DBContext.SaveChanges();
+                ViewBag.Messsage = "Record Create Successfully";
+                return RedirectToAction("Index");
+            }
+          
+            return View();
+        }
+
+
+
+
+    }
+
+      
+
+    
+}
---
+++ TASKTRACKERSYSTEM/CustomValidation/Alphanumeric.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace TASKTRACKERSYSTEM.CustomValidation
+{
+    public class AlphanumericAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value != null)
+            {
+                string stringValue = value.ToString();
+
+                
+                if (!Regex.IsMatch(stringValue, "^[a-zA-Z0-9]*$") || stringValue.Length > 50)
+                {
+                    return new ValidationResult("Task Name should allow only alphanumeric characters and be maximum 50 characters.");
+                }
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
